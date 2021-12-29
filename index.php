64c5dce715e16e00ec385127571cf9413351b36a
vovka013 <46564695+vovka013@users.noreply.github.com>
2021-12-29 15:35:05 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,24 @@ header('Content-type: json/application');
 require_once "connect.php";
 require_once "function.php";
 
+if(!isset($_SERVER['PHP_AUTH_USER'])){
+    header("WWW-Authenticate: Basic realm=\"Privar Area\"");
+    header("HTTP/1.0 401 Unauthorized");
+    print("Sorry, you need proper credentials");
+    exit();
+
+}else{
+    if($_SERVER['PHP_AUTH_USER']=="admin" && $_SERVER['PHP_AUTH_PW']=="qwe"){
+        print "You in privat area";
+
+    }else{
+        header("WWW-Authenticate: Basic realm=\"Privar Area\"");
+        header("HTTP/1.0 401 Unauthorized");
+        print("Sorry, you need proper credentials");
+        exit();
+    }
+}
+
 $method = $_SERVER['REQUEST_METHOD'];
 
 $q=$_GET['q'];
